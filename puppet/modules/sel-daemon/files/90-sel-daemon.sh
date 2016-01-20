75b502dab8bced9d65ab792ce2cf4c5be96c3cb7
danieleguttadoro <daniele.guttadoro@gmail.com>
2016-01-20 18:31:17 +0100

    Stable daemon config version

--- puppet/modules/sel-daemon/files/90-sel-daemon.sh
+++ puppet/modules/sel-daemon/files/90-sel-daemon.sh
@@ -5,9 +5,12 @@ if is_service_enabled sel-daemon; then
     if [[ "$1" == "source" ]]; then
         # Initial source
         source $TOP_DIR/lib/sel-daemon
-	elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
-        echo_summary "Configuring SEL-Daemon"
-        configure_sel-daemon
+	#elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+    #    echo_summary "Installing SEL-Daemon"
+    #    install_sel-daemon
+	#elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+    #    echo_summary "Configuring SEL-Daemon"
+    #    configure_sel-daemon
     elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
         echo_summary "Initializing SEL-Daemon"
         start_sel-daemon
